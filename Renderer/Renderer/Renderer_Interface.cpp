44ca287d29cbb449b9d87334216878d10a1ae67e
dastyk <dastykill@gmail.com>
2018-01-04 09:55:59 +0100

    Create a dx11 instance

--- Renderer/Renderer/Renderer_Interface.cpp
+++ Renderer/Renderer/Renderer_Interface.cpp
@@ -1,6 +1,14 @@
 #include <Graphics\Renderer_Interface.h>
-
+#include "Renderer_DX11.h"
 DECLDIR_GRAPHICS_C Graphics::Renderer_Interface * CreateRenderer(Renderer_Backend backend)
 {
+	switch (backend)
+	{
+	case Renderer_Backend::DIRECTX11:
+		return new Graphics::Renderer_DX11();
+		break;
+	default:
+		break;
+	}
 	return nullptr;
 }
